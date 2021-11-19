21491bb4d35721c53f69309e5b498b037e4eab72
Jonathan Sewell <jsewellro@gmail.com>
2021-11-19 09:58:57 -0400

    test

--- Develop/logic.js
+++ Develop/logic.js
@@ -20,10 +20,11 @@ var questionEl = document.getElementById("question");
 function getQuiz() {
 
 };
-var arr = [{question: "Commonly used data types DO NOT include: ", answers: ["strings", "booleans", "alerts", "numbers"],correctAnswer: "alerts"},
-{question: "Commonly used data types DO NOT include: ", answers: ["strings", "booleans", "alerts", "numbers"],correctAnswer: "alerts"},
-{question: "Commonly used data types DO NOT include: ", answers: ["strings", "booleans", "alerts", "numbers"],correctAnswer: "alerts"},
-{question: "Commonly used data types DO NOT include: ", answers: ["strings", "booleans", "alerts", "numbers"],correctAnswer: "alerts"}];
+var questionArr = [{question: "Commonly used data types DO NOT include: ", answers: ["strings", "booleans", "alerts", "numbers"],correctAnswer: "alerts"},
+{question: "The condition in an if / else statement is enclosed within ____.  ", answers: ["quotes", "curly brackets", "parentheses", "square brackets "],correctAnswer: "parentheses"},
+{question: "Arrays in JavaScript can be used to store ____.", answers: ["numbers and strings","other arrays","booleans","all of the above" ],correctAnswer: "all of the above"},
+{question: "String values must be enclosed within ____ when being assigned to variables.  ", answers: ["commas","curly brackets","quotes", "parentheses"],correctAnswer: "quotes"},
+{question: "A very useful tool used during development and debugging for printing content to the debugger is:", answers: ["JavaScript  terminal","/ bash","for loops", "console.log"],correctAnswer: "console.log"}];
 
 
       
@@ -99,7 +100,7 @@ function userClick(){
 }
 
 function getNextQuestion(){
-  let currentQuestion = arr[currentQuestionIndex];
+  let currentQuestion = questionArr[currentQuestionIndex];
   questionEl.textContent = currentQuestion.question;
   answersdivEl.innerHTML = "";
   currentQuestion.answers.forEach(function(answer,idx)
